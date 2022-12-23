d4474cb72dccc57e6c5ca42c72ba109d1d5308f0
p-olszewski <przemo1297@gmail.com>
2022-12-24 00:03:49 +0100

    Add sample text to test the TextView in the FirstFragment

--- app/src/main/java/com/example/weatherapp/view/FirstFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/FirstFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 
@@ -13,6 +14,9 @@ class FirstFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.first_fragment, container, false)
+        val view = inflater.inflate(R.layout.first_fragment, container, false)
+        val testTemp = view.findViewById<TextView>(R.id.tvTemp)
+        testTemp.text = "123"
+        return view
     }
 }
