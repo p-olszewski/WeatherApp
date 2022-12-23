3fb6d218ca18dbfbc503be28e434bc4ee48b1d9b
p-olszewski <przemo1297@gmail.com>
2022-12-23 10:42:46 +0100

    Add refresh data

--- app/src/main/java/com/example/weatherapp/view/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/view/MainActivity.kt
@@ -5,7 +5,10 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
 import android.widget.Toast
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.viewpager2.widget.ViewPager2
 import com.example.weatherapp.R
@@ -26,15 +29,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        GET = getSharedPreferences(packageName, MODE_PRIVATE)
-        SET = GET.edit()
-
-        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
-
         // variables and constants
         val tabLayout = findViewById<TabLayout>(R.id.tabLayout)
         val viewPager = findViewById<ViewPager2>(R.id.viewPager)
+        val tvCityName = viewPager.findViewById<TextView>(R.id.tvCityName)
+        val tvTemp = viewPager.findViewById<TextView>(R.id.tvTemp)
+        val tvCoords = viewPager.findViewById<TextView>(R.id.tvCoords)
+        val tvPressure = viewPager.findViewById<TextView>(R.id.tvPressure)
+        val tvRefreshTime = viewPager.findViewById<TextView>(R.id.tvRefreshTime)
+        val ivWeatherImage = viewPager.findViewById<TextView>(R.id.ivWeatherImage)
         val addFAB = findViewById<FloatingActionButton>(R.id.idFABAdd)
         val refreshFAB = findViewById<FloatingActionButton>(R.id.idFABRefresh)
         val saveFAB = findViewById<FloatingActionButton>(R.id.idFABSave)
@@ -43,6 +46,16 @@ class MainActivity : AppCompatActivity() {
         // sharedPreferences
 //        val sharedPref = getSharedPreferences("myPref", MODE_PRIVATE)
 //        sharedPref.edit()
+
+        GET = getSharedPreferences(packageName, MODE_PRIVATE)
+        SET = GET.edit()
+
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        var cityName = GET.getString("cityName", "Lodz")
+        tvCityName.text = cityName
+        viewModel.refreshData()
+        getLiveData()
+
 
         // ViewPager
         val adapter = ViewPagerAdapter(supportFragmentManager, lifecycle)
@@ -79,5 +92,13 @@ class MainActivity : AppCompatActivity() {
         saveFAB.setOnClickListener {
             Toast.makeText(this@MainActivity, "Save clicked..", Toast.LENGTH_SHORT).show()
         }
+    }
+
+    private fun getLiveData() {
+        viewModel.weatherData.observe(this, Observer { data ->
+            data?.let {
+
+            }
+        })
     }
 }
