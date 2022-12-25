a9e29a4ff0615a75b696a9e72a552a6ef2dac762
p-olszewski <przemo1297@gmail.com>
2022-12-25 11:25:49 +0100

    Add public method updateCityName in the FirstFragment class

--- app/src/main/java/com/example/weatherapp/view/FirstFragment.kt
+++ app/src/main/java/com/example/weatherapp/view/FirstFragment.kt
@@ -9,14 +9,30 @@ import androidx.fragment.app.Fragment
 import com.example.weatherapp.R
 
 class FirstFragment : Fragment() {
+//    lateinit var tvCityName: TextView
+//    lateinit var tvTemp: TextView
+//    lateinit var tvCoords: TextView
+//    lateinit var tvPressure: TextView
+//    lateinit var tvRefreshTime: TextView
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.first_fragment, container, false)
-        val testTemp = view.findViewById<TextView>(R.id.tvTemp)
-        testTemp.text = "123"
+//        val testTemp = view.findViewById<TextView>(R.id.tvTemp)
+//        testTemp.text = "123"
         return view
+    }
+
+    fun updateCityName(text: String) {
+        val tvCityName = view?.findViewById<TextView>(R.id.tvCityName)
+//        val tvTemp = view?.findViewById<TextView>(R.id.tvTemp)
+//        val tvCoords = view?.findViewById<TextView>(R.id.tvCoords)
+//        val tvPressure = view?.findViewById<TextView>(R.id.tvPressure)
+//        val tvRefreshTime = view?.findViewById<TextView>(R.id.tvRefreshTime)
+
+        tvCityName?.text = text
     }
 }
